e4cb0ea36873c42009963c26504cff66131f53a7
ajdx <biriulintema@yandex.ru>
2020-04-25 17:58:14 +0300

    fix

--- run.py
+++ run.py
@@ -4,7 +4,10 @@ from flask import Flask
 from data import db_session
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+SECRET_KEY = os.getenv('SECRET_KEY', 'vselfie_secret_key')
+APP_ENV = os.getenv('APP_ENV', 'development')
+app.config['SECRET_KEY'] = SECRET_KEY
+
 
 from views import general
 app.register_blueprint(general.mod)
@@ -12,6 +15,8 @@ app.register_blueprint(general.mod)
 db_session.global_init("db/vselfie.sqlite")
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    #app.run(port=port, host='0.0.0.0')
-    app.run(port=8080, host='127.0.0.1', debug=True, use_reloader=True, threaded=True)
+    if APP_ENV == 'production':
+        port = int(os.environ.get("PORT", 8080))
+        app.run(port=port, host='0.0.0.0')
+    else:
+        app.run(port=8080, host='127.0.0.1', debug=True, use_reloader=True, threaded=True)
